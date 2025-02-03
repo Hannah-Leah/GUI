f510e247c1d4a9c2031339bede374af27f3e3466
HannahLeah <Hannah.Leah@Oebb.at>
2025-02-03 12:37:06 +0100

    Added a new project for CSV

---
+++ CSVLoader/CSVConvertor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace CSVLoader
+{
+    internal static class CSVConvertor
+    {
+        public static string[] outputCSV(ObservableCollection<Person> persons, string seperator)
+        {
+            // +1 because we need to include the headers
+            string[] tempArr = new string[persons.Count + 1];
+            tempArr[0] = "Vorname{seperator}NachName{seperator}Email{seperator}Birthday{seperator}ID";
+            for (int count = 0; count < persons.Count; count++)
+            {
+                tempArr[count + 1] = $"{persons[count].Name} {seperator} {persons[count].LastName} {seperator} " +
+                    $"{persons[count].Email} {seperator} {persons[count].Birthday} {seperator} {persons[count].ID}";
+            }
+            return tempArr;
+        }
+
+        public static ObservableCollection<Person> ImportCSV(string[] importText, string seperator)
+        {
+            ObservableCollection<Person> tempList = new ObservableCollection<Person>();
+            if (importText.Length > 1)
+            {
+                for (int count = 1;count < importText.Length; count++) 
+                {
+                    // Max;Muster;mm@gmail.com;01.01.2020;123
+                    string[] tempString = importText[count].Split(seperator);
+                    if (tempString.Length != 5)
+                    {
+                        throw new FormatException("CSV is in the wrong format or empty");
+                    }
+
+                    Person tempPerson = new Person();
+                    tempPerson.Name = tempString[0];
+                    tempPerson.LastName = tempString[1];
+                    tempPerson.Email = tempString[2];
+                    tempPerson.Birthday = DateTime.Parse(tempString[3]);
+                    int test;
+                    int.TryParse(tempString[4], out test);
+                    tempPerson.ID = test;
+
+                    tempList.Add(tempPerson);
+                }
+
+               
+            }
+            return tempList;
+        }
+    }
+}
---
+++ CSVLoader/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSVLoader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Person> _persons { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            _persons = new ObservableCollection<Person>();
+            DataContext = this;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Ensure that SeperatorSelector has a valid selection
+            string seperator = string.Empty;
+
+            if (SeperatorSelector.SelectedItem != null)
+            {
+                seperator = ((ComboBoxItem)SeperatorSelector.SelectedItem).Content.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please select a separator.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Check if the user has entered a custom separator
+            if (seperator == "Choose your own")
+            {
+                // Ensure the user has entered a valid custom separator
+                if (string.IsNullOrEmpty(tbxCustomSeparator.Text))
+                {
+                    MessageBox.Show("Please enter a custom separator.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                seperator = tbxCustomSeparator.Text;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = ".csv";
+            saveFileDialog.Filter = "CSV file (*.csv)|*.csv|Text file (*.txt)|*.txt|All files (*.*)|*.* ";
+            saveFileDialog.AddExtension = true;
+            if (saveFileDialog.ShowDialog() == true)
+            {
+               File.WriteAllLines(saveFileDialog.FileName, CSVConvertor.outputCSV(_persons, seperator));
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = new Person();
+            person.Name = tbxName.Text;
+            person.LastName = tbxLastName.Text;
+            person.Email = tbxEmail.Text;
+            person.Birthday = dtpBirthday.SelectedDate == null ? new DateTime() : (DateTime) dtpBirthday.SelectedDate;
+            int temp = 0;
+            int.TryParse(tbxID.Text, out temp);
+            person.ID = temp;
+
+            _persons.Add(person);
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+            string seperator = string.Empty;
+
+            if (SeperatorSelector.SelectedItem != null)
+            {
+                seperator = ((ComboBoxItem)SeperatorSelector.SelectedItem).Content.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please select a separator.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "CSV file (*.csv)|*.csv|Text file (*.txt)|*.txt|All files (*.*)|*.* "; ;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+
+
+                    foreach (var item in CSVConvertor.ImportCSV(File.ReadAllLines(openFileDialog.FileName), seperator))
+                    {
+                        _persons.Add(item);
+                    }
+
+                }
+                catch
+                {
+                    MessageBox.Show("Something went wrong.");
+                    return;
+                }
+                
+            }
+        }
+    }
+}
